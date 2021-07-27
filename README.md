484f63e5fe9ac932953ee5078daca5c26d9befab
renhuabin <renhuabin@123.cn>
2021-07-27 21:58:44 +0800

    dajian

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # maoyan
 
-## Project setup
+## 技术要点
 ```
-npm install
-```
+1. vuex     状态管理
+2. vue-router   路由
+3. keep-alive
+4. swiper
+5. better-scroll 
+6. 图片的懒加载
+7. 路由懒加载
+8. 移动端事件  vue-touch
+9. mint-ui
+10. 封装JS组件
+11. 封装UI组件
+12. sass
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+npm install better-scroll swiper mint-ui vue-touch@next -S
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+npm install vue-lazyload -D
+
+```
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,28 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import VueRouter from "vue-router"
+import movie from './movie'
+import mine from './mine'
+import cinema from './cinema'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  
+ 
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  mode: 'hash',
+ 
+  routes:[
+    {
+      path:'/',
+      redirect:'/movie'
+    },
+    movie,
+    mine,
+    cinema
+  ]
 })
 
 export default router
